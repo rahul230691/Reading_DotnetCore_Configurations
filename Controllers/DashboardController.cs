b7179e5df435386184b12a0c8d5c0c301e0fe350
Rahul Saraswat <rahulsharma.112111@hotmail.com>
2020-04-13 03:05:08 +0530

    First Commit - How to read settings using IConfiguration

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace dotnetcore_configurations.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        //requires using Microsoft.Extensions.Configuration
+        public DashboardController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            List<object> listSettings = new List<object>();
+
+            //var dashboardTitle = configuration.GetValue<string>("DashboardSettings:Title");
+
+            var headerSection = configuration.GetSection("DashboardSettings:Header");
+            var isSearchBoxEnabled = headerSection.GetValue<bool>("IsSearchBoxEnabled");
+            var headerBannerTitle = headerSection.GetValue<string>("BannerTitle");
+            var isBannerSliderEnabled = headerSection.GetValue<bool>("IsBannerSliderEnabled");
+
+            listSettings.AddRange(new object[] { isSearchBoxEnabled, headerBannerTitle, isBannerSliderEnabled });
+
+            //requires using Newtonsoft.Json
+            return Content(JsonConvert.SerializeObject(listSettings));
+        }
+    }
+}
